a75759f9a86b14aa23476bb3cfc630c46c862fa5
TengWei-Hung <32382580+TengWei-Hung@users.noreply.github.com>
2017-10-02 21:31:25 +0800

    Upload question2
    Upload question2

---
+++ InwellcomeInterview Questions/Test1/Test2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string strNum = string.Empty;
+                int Num = 0;
+
+                do
+                {
+                    Console.WriteLine("Please enter the number : ");
+                    strNum = Console.ReadLine();
+                } while (!Int32.TryParse(strNum, out Num));
+
+                int loop = 1;
+                for (int i = 1; i < Num + 1; i++)
+                    loop *= i;
+
+                Console.WriteLine(string.Format("For loop : {0}! = {1}", strNum, loop));
+
+                Console.WriteLine(string.Format("Factorial : {0}! = {1}", strNum, factorial(Num)));
+            }
+        }
+
+        private static int factorial(int num)
+        {
+            if (num == 1)
+                return 1;
+            else
+                return num * factorial(num - 1);
+        }
+    }
+}
