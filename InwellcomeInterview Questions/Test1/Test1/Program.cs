2ceb0c46faa558b77b634dac9716cb6e4ddfaf7b
TengWei-Hung <32382580+TengWei-Hung@users.noreply.github.com>
2017-10-02 20:52:09 +0800

    Add files via upload

---
+++ InwellcomeInterview Questions/Test1/Test1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = (5 << 4) >> 2;
+            Console.WriteLine("(5 << 4) >> 2 = " + a);
+
+            int b = 5 << 4;
+            Console.WriteLine("5 << 4 = " + b);
+
+            int c = 80 >> 2;
+            Console.WriteLine("80 >> 2 = " + c);
+
+            int d = 5 << (4 >> 2);
+            Console.WriteLine("(5 << (4 >> 2) = " + d);
+
+            int e = 4 >> 2;
+            Console.WriteLine("4 >> 2 = " + e);
+
+            int f = 5 << 1;
+            Console.WriteLine("5 << 1 = " + f);
+
+            Console.ReadKey();
+        }
+    }
+}
